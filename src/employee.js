a1d98e413d1568da4fc46ae3b4c2e210dc9f4a24
Kheanne Miguel <162478783+m-kian@users.noreply.github.com>
2025-05-31 13:14:59 +0800

    Create employee.js

---
+++ src/employee.js
@@ -0,0 +1,12 @@
+import { supabase } from './supabase.js';
+
+export async function fetchEmployees() {
+    const { data, error } = await supabase.from('employees').select('*');
+
+    if (error) {
+        console.error('Error fetching employees:', error.message);
+        return [];
+    }
+
+    return data;
+}
